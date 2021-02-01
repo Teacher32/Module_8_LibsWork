944106a4f9870d5fea047b880aebcb41a2e4e31a
Teacher32 <kitaev-yuri@yandex.ru>
2021-02-01 10:56:25 +0300

    Добавлен файл Test и директория Testfiles.

--- src/com/libs/files/Main.java
+++ src/com/libs/files/Main.java
@@ -1,12 +1,15 @@
 package com.libs.files;
 
 import java.io.File;
+import java.util.Date;
 // import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) /*throws IOException*/ {
         File file = new File("Testfiles/Test.txt");
+
+        // Проверяем, существует ли файл?
         boolean isExsists = file.exists();
         if (isExsists) {
             System.out.println("Файл с именем " + file.getName() + " существует.");
@@ -16,6 +19,23 @@ public class Main {
             return;
         }
 
+        // Выводим абсолютный путь к файлу
         System.out.println("Абсолютный путь к файлу " + file.getName() + " " + file.getAbsolutePath());
+
+        // Проверяем, доступен ли файл для чтения/записи?
+        boolean isRead = file.canRead();
+        boolean isWrite = file.canWrite();
+        if (isRead && isWrite) {
+            System.out.println("Файл " + file.getName() + " доступен для чтения/записи.");
+        }
+        else {
+            System.out.println("Файл " + file.getName() + " не доступен для редактирования.");
+        }
+
+        // Выводим дату и время последнего изменения
+        long ms = file.lastModified();
+        Date date = new Date(ms);
+        System.out.println("Дата последнего изменения файла " + file.getName() + ": " + date);
+
     }
 }
